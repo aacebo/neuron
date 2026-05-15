0f390303d97ff02213908dbcfcb7d1fbe7591417
Alex Acebo <aacebowork@gmail.com>
2026-05-15 08:14:45 -0700

    dockerize

--- bins/executor/src/main.rs
+++ bins/executor/src/main.rs
@@ -1,4 +1,30 @@
+mod config;
+
+use amqp::{Action, Key};
+
+use config::Config;
+
 #[tokio::main]
-async fn main() {
-    println!("Hello, world!");
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let config = Config::from_env();
+    let socket = amqp::new(&config.rabbitmq_url)
+        .with_app_id("sigmod[worker]")
+        .with_queue(Key::new("workspace", Action::Create))
+        .connect()
+        .await?;
+
+    let mut consumer = socket
+        .consume(Key::new("workspace", Action::Create))
+        .await?;
+
+    println!("waiting for messages on workspace.create...");
+
+    while let Some(res) = consumer.dequeue::<String>().await {
+        let _ = match res {
+            Err(err) => return Err(err.into()),
+            Ok(v) => v,
+        };
+    }
+
+    Ok(())
 }
