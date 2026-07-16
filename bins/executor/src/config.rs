8d924e688ddd86fed8928c7dc5e185957896c44d
aacebo <aacebowork@gmail.com>
2026-07-16 13:31:01 -0400

    Update config.rs

--- bins/executor/src/config.rs
+++ bins/executor/src/config.rs
@@ -15,7 +15,6 @@ impl Config {
             .expect("PORT must be a valid number");
 
         let database_url = env::var("DATABASE_URL").unwrap_or_else(|_| "postgres://admin:admin@localhost:5432/main".to_string());
-
         let rabbitmq_url = env::var("RABBITMQ_URL").unwrap_or_else(|_| "amqp://admin:admin@localhost:5672".to_string());
 
         Self {
